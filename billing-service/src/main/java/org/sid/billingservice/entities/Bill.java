8fd9bbb7f5a64b2dff05dbbfa2a629e552379d39
Arij <arijmetouekel123@gmail.com>
2024-11-17 20:45:59 +0100

    first commit

---
+++ billing-service/src/main/java/org/sid/billingservice/entities/Bill.java
@@ -0,0 +1,21 @@
+package org.sid.billingservice.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.sid.billingservice.model.Customer;
+
+import java.util.Date;
+import java.util.List;
+@Entity @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter @Setter
+public class Bill {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date billDate;
+    private Long customerId;
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems;
+    @Transient
+    private Customer customer;
+}
+
---
+++ billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
@@ -0,0 +1,24 @@
+package org.sid.billingservice.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+import org.sid.billingservice.model.Product;
+
+
+@Entity @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@Setter
+public class ProductItem {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Bill bill;
+    private int quantity;
+    private double price;
+    private double discount;
+    @Transient
+    private Product product;
+}
